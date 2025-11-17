1af123121c0fc2a27c033815bede3e9268f353b6
mcedronmer <mcedronmer@educacion.navarra.es>
2025-11-17 13:38:10 +0100

    Update Main.java

--- UT3_A5_Hospital/src/main/java/Main.java
+++ UT3_A5_Hospital/src/main/java/Main.java
@@ -17,12 +17,21 @@ public class Main {
         historialMedicoService.create(historialMedico);
         System.out.println("HistorialMedico DESPUÉS de create:\n" + historialMedico);
 
+        HistorialMedico historialMedico1 = new HistorialMedico();
+        historialMedico1.setAlergias("Graminias");
+        historialMedico1.setVacunas("Gripe");
+        historialMedicoService.create(historialMedico1);
+        HistorialMedico historialMedico2 = new HistorialMedico();
+        historialMedico2.setEnfermedades("Hipertension");
+        historialMedico2.setMedicamentos("Paracetamol");
+        historialMedicoService.create(historialMedico2);
+
         List<HistorialMedico> historialMedicoList = historialMedicoService.findAll();
         System.out.println("TODOS LOS HistorialMedico PERSISTIDOS:");
         historialMedicoList.forEach(System.out::println);
 
-        List<HistorialMedico> historialMedico1 = historialMedicoService.findDiabeticPeople("Diabetes");
-        historialMedico1.forEach(System.out::println);
+        List<HistorialMedico> historialMedicoList1 = historialMedicoService.findDiabeticPeople("Diabetes");
+        historialMedicoList1.forEach(System.out::println);
 
         Habitacion habitacion1 = new Habitacion();
         habitacion1.setNumero(120);
@@ -36,14 +45,17 @@ public class Main {
         Habitacion habitacion2 = new Habitacion();
         habitacion2.setNumero(120);
         habitacion2.setDisponible(false);
+        habitacionService.create(habitacion2);
 
         Habitacion habitacion3 = new Habitacion();
         habitacion3.setNumero(120);
         habitacion3.setDisponible(true);
+        habitacionService.create(habitacion3);
 
         Habitacion habitacion4 = new Habitacion();
         habitacion4.setNumero(120);
         habitacion4.setDisponible(false);
+        habitacionService.create(habitacion4);
 
         List<Habitacion> habitacionList = habitacionService.findAll();
         System.out.println("TODAS LAS HABITACIONES: ");
